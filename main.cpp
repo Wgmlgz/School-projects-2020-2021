ad31964f179f346873b8d48b7c5b127af18c2aab
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2020-09-25 12:28:54 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
 struct XYZ {
-    int x, y, z;
+    int64_t x, y, z;
 };
 struct ABCD {
-    int a, b, c, d;
+    int64_t a, b, c, d;
 };
 
 bool cmp_XYZ(XYZ a, XYZ b) {
@@ -13,8 +13,8 @@ bool cmp_XYZ(XYZ a, XYZ b) {
 
     return f;
 }
-XYZ RNG (XYZ x, ABCD a) {
-    int res = (a.a*x.x + a.b*x.y + a.c*x.z) % a.d;
+XYZ RNG(XYZ x, ABCD a) {
+    int64_t res = (a.a * x.x + a.b * x.y + a.c * x.z) % a.d;
 
     x.z = x.y;
     x.y = x.x;
@@ -23,11 +23,11 @@ XYZ RNG (XYZ x, ABCD a) {
     return x;
 }
 
-int main () {
+int main() {
 
 
     // result values
-    int p = 1, n = 0;
+    int64_t p = 1, n = 0;
     double z = 0;
 
 
@@ -39,49 +39,49 @@ int main () {
 
     // p find
     XYZ frog = x, toad = x;
-    for (int i = 0; !cmp_XYZ(toad, frog) || i == 0; ++i) {
+    for (int64_t i = 0; !cmp_XYZ(toad, frog) || i == 0; ++i) {
         frog = RNG(frog, a);
         frog = RNG(frog, a);
         toad = RNG(toad, a);
     }
 
     frog = RNG(frog, a);
-    for (;!cmp_XYZ(toad, frog); frog = RNG(frog, a), ++p) ;
-    
+    for (; !cmp_XYZ(toad, frog); frog = RNG(frog, a), ++p);
+
 
     // n find
     frog = x, toad = x;
-    for (int i = 0; i < p; i++, frog = RNG(frog, a)) ;
-    
-    for (int i = 0; !cmp_XYZ(toad, frog); ++i, ++n) {
-                frog = RNG(frog, a);
+    for (int64_t i = 0; i < p; i++, frog = RNG(frog, a));
+
+    for (int64_t i = 0; !cmp_XYZ(toad, frog); ++i, ++n) {
+        frog = RNG(frog, a);
         toad = RNG(toad, a);
     }
-    
-    
+
+
     // z find
-    int range_vals[20];
+    int64_t range_vals[20];
     XYZ tmp_xyz = x;
 
-    for (int i = 0; i < 20; i++) {
+    for (int64_t i = 0; i < 20; i++) {
         range_vals[i] = 0;
     }
 
-    for (int i = 0; i < 400; i++) {
+    for (int64_t i = 0; i < 400; i++) {
+
+        //std::cout << tmp_xyz.z << std::endl;
+
+        int64_t tmp = (tmp_xyz.z * 20) / a.d;
+        ++range_vals[tmp];
         tmp_xyz = RNG(tmp_xyz, a);
-
-        std::cout << tmp_xyz.z << std::endl;
-
-        int tmp = tmp_xyz.x;
-        double tmpf = static_cast<double>(tmp) / a.d; 
-        ++range_vals[static_cast<int>(tmpf * 20)];
     }
 
-    for (int i = 0; i < 20; i++) {
-        z += pow(static_cast<double>(range_vals[i] - 20), 2) / 400;
+    for (int64_t i = 0; i < 20; i++) {
+        z += (double)((range_vals[i] - 20) * (range_vals[i] - 20)) / 400.0;
     }
-    
+
 
     // output
+    std::cout << std::setprecision(4);
     std::cout << p << " " << n << " " << z;
 }
