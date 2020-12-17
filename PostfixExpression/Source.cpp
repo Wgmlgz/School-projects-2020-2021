954ff0165ab22c0f8ff2e9a3abfaceee060edffb
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2020-12-18 01:01:21 +0300

    Update Source.cpp

--- PostfixExpression/Source.cpp
+++ PostfixExpression/Source.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 int main() {
   std::vector<string> tests = {
-      "(1+2*(3^4^5+6*sin(3-2*-4))+2)/3"};
+      "sin(9*2*5)"};
   for (auto s : tests) {
     std::cout << ConvertToPostfix(s) << std::endl;
   }
